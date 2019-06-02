947de1f4a7060991b54a4ad6718a9728b2e04933
ShelaevNikita <shelaev.nr@edu.spbstu.ru>
2019-06-03 00:51:16 +0300

    Task3

--- src/core/Ships.kt
+++ src/core/Ships.kt
@@ -5,3 +5,4 @@ enum class Ships {
     FOURTH,
     NO;
 }
+
--- test/BoardTest.kt
+++ test/BoardTest.kt
@@ -2,7 +2,6 @@ import org.junit.Assert.*
 import org.junit.Test
 
 class BoardTest {
-
     @Test
     fun control() {
         val board = Board(10, 10)
